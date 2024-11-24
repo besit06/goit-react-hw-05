65ffd0d0505224a78e05f1691adeb755bcaef1a8
besit06 <behemoth2054@gmail.com>
2024-11-24 20:21:56 +0100

    goit-react-hw-05

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import { Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage/HomePage";
-import MoviesPage from "./pages/MoviesPage/MoviesPage";
-import MovieDetailsPage from "./pages/MovieDetailsPage/MovieDetailsPage";
-import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
-import Navigation from './components/Navigation/Navigation';
-import MovieCast from './components/MovieCast/MovieCast';
-import MovieReviews from './components/MovieReviews/MovieReviews';
+import { lazy, Suspense } from 'react';
+
+const HomePage = lazy(() => import("./pages/HomePage/HomePage"))
+const MoviesPage = lazy(() => import("./pages/MoviesPage/MoviesPage"))
+const MovieDetailsPage = lazy(() => import("./pages/MovieDetailsPage/MovieDetailsPage"))
+const Navigation = lazy(() => import("./components/Navigation/Navigation"))
+const MovieCast = lazy(() => import("./components/MovieCast/MovieCast"))
+const MovieReviews = lazy(() => import("./components/MovieReviews/MovieReviews"))
+const NotFoundPage = lazy(() => import("./pages/NotFoundPage/NotFoundPage"))
+
 
 const App = () => {
   return (
-    <>
+    <>  <Suspense>
       <Navigation />
-      <Routes>
+            <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/movies" element={<MoviesPage />} />
         <Route path="/movies/:movieId/*" element={<MovieDetailsPage />}>
@@ -20,6 +23,7 @@ const App = () => {
         </Route>
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
+      </Suspense>
     </>
   );
 }
